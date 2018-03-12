8532a14d2b0647530575d8155a8eaed41775579c
asambolec <andreja.sambolec@gmail.com>
2018-03-12 12:17:26 +0100

    Revert "Revert "Revert "Test"""

--- src/main/java/eu/jsparrow/service/Service.java
+++ src/main/java/eu/jsparrow/service/Service.java
@@ -5,6 +5,7 @@ public class Service {
 	static int i = 1;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
         
         i = i + 1;
     }
