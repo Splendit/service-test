81dc46745c31ec9fd5454fb7cc88bfe3a82e1fc4
Andreja <andreja.sambolec@splendit.at>
2018-09-05 12:55:07 +0200

    SIM-1260 ADD diamond refactoring

--- src/src/main/java/Test.java
+++ src/src/main/java/Test.java
@@ -1,10 +1,16 @@
 package src.main.java;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Test {
 
 	static 
 	int i = 1;
-	
+
+    List<String> list = new ArrayList<String>();	
+
+
 	public static void main(String[] args) {
 		System.out.println("Hello!!");
 	
